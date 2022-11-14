e75d761ab43f90141c918c26267f11668665cef5
Sergey Evseev <sevseev@gmail.com>
2022-11-14 19:45:09 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        private Player player;
+        int speed = 2;
+        Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += new KeyEventHandler(press);
+            player = new Player(new Point(this.Width / 2, this.Height / 2));
+            FormClosing += new FormClosingEventHandler(close);
+            timer = new Timer(1);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            updateGame();
+        }
+
+        public void close(object sender, FormClosingEventArgs args)
+        {
+            player.stop();
+            timer.Stop();
+            timer.Dispose();
+        }
+        protected override void OnPaint(PaintEventArgs args)
+        {
+            player.paint(args.Graphics);
+        }
+
+        public void press(object sender, KeyEventArgs key)
+        {
+            if(player.currentDirection() == Direction.Stop)
+            {
+                player.setDirection(key.KeyCode);
+                player.start();
+            }
+            else
+            {
+                player.setDirection(key.KeyCode);
+            }
+            
+        }
+
+        private void updateGame()
+        {
+            Graphics graphics = CreateGraphics();
+            graphics.Clear(BackColor);
+            player.paint(graphics);
+        }
+        public void print(object e, PaintEventArgs args)
+        {
+            MessageBox.Show("swrfvg");
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    Graphics graphics = this.CreateGraphics();
+        //    graphics.Clear(BackColor);
+        //    Point point = new Point((int)numericUpDown1.Value, (int)numericUpDown2.Value);
+        //    Point point1 = new Point((int)numericUpDown3.Value, (int)numericUpDown4.Value);
+        //    graphics.DrawLine(new Pen(Brushes.Orange, 3), point, point1);
+        //}
+    }
+}
---
+++ WindowsFormsApp9/Player.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down,
+        Stop
+    }
+    internal class Player
+    {
+        private Point point;
+        Direction directionState;
+        Thread thread;
+        bool isRunning;
+        int speed;
+        public Player(Point point)
+        {
+            this.point = point;
+            directionState = Direction.Stop;
+            isRunning = false;
+            speed = 10;
+            thread = new Thread(move);
+        }
+
+        public Direction currentDirection()
+        {
+            return directionState;
+        }
+
+        public void setDirection(Keys key)
+        {
+            switch (key)
+            {
+                case Keys.Left:
+                    directionState = Direction.Left;
+                    break;
+                case Keys.Right:
+                    directionState = Direction.Right;
+                    break;
+                case Keys.Up:
+                    directionState = Direction.Up;
+                    break;
+                case Keys.Down:
+                    directionState = Direction.Down;
+                    break;
+            }
+        }
+
+        private void move()
+        {
+            if (directionState != Direction.Stop)
+            {
+                while (isRunning)
+                {
+                    switch (directionState)
+                    {
+                        case Direction.Left:
+                            moveX(-speed);   
+                            break;
+                        case Direction.Right:
+                            moveX(speed);
+                            break;
+                        case Direction.Down:
+                            moveY(speed);
+                            break;
+                        case Direction.Up:
+                            moveY(-speed);
+                            break;
+                    }
+                    Thread.Sleep(50);
+                }
+            }
+        }
+
+        public void start()
+        {
+            isRunning = true;
+            thread.Start();
+        }
+
+        public void stop()
+        {
+            isRunning=false;
+        }
+        public void moveX(int dx)
+        {
+            point.X = point.X + dx;
+        }
+
+        public void moveY(int dy)
+        {
+            point.Y = point.Y + dy;
+        }
+
+        public void paint(Graphics graphics)
+        {
+            Rectangle rect = new Rectangle(point, new Size(50, 50));
+            graphics.FillRectangle(new SolidBrush(Color.Red), rect);
+        }
+    }
+}
